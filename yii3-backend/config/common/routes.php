3180c4d1d7f1ec2cd76cba291393c4a01ee4f40d
ivan-s-2001 <ivan.s.2001@yandex.com>
2026-07-20 18:33:07 +0300

    Register user and group APIs

--- yii3-backend/config/common/routes.php
+++ yii3-backend/config/common/routes.php
@@ -18,10 +18,24 @@ use App\Api\DocumentsMoveAction;
 use App\Api\DocumentsRestoreAction;
 use App\Api\DocumentsSearchAction;
 use App\Api\DocumentsUpdateAction;
+use App\Api\GroupsAddUserAction;
+use App\Api\GroupsCreateAction;
+use App\Api\GroupsDeleteAction;
+use App\Api\GroupsInfoAction;
+use App\Api\GroupsListAction;
+use App\Api\GroupsRemoveUserAction;
+use App\Api\GroupsUpdateAction;
+use App\Api\GroupsUsersListAction;
 use App\Api\HealthAction;
 use App\Api\InstallationCreateAction;
 use App\Api\InstallationInfoAction;
 use App\Api\RevisionsListAction;
+use App\Api\UsersCreateAction;
+use App\Api\UsersDeleteAction;
+use App\Api\UsersInfoAction;
+use App\Api\UsersListAction;
+use App\Api\UsersSuspendAction;
+use App\Api\UsersUpdateAction;
 use Yiisoft\Router\Route;
 
 return [
@@ -34,6 +48,22 @@ return [
 
     Route::post('/api/installation.create')->action(InstallationCreateAction::class)->name('api/installation.create'),
     Route::post('/api/installation.info')->action(InstallationInfoAction::class)->name('api/installation.info'),
+
+    Route::post('/api/users.list')->action(UsersListAction::class)->name('api/users.list'),
+    Route::post('/api/users.info')->action(UsersInfoAction::class)->name('api/users.info'),
+    Route::post('/api/users.create')->action(UsersCreateAction::class)->name('api/users.create'),
+    Route::post('/api/users.update')->action(UsersUpdateAction::class)->name('api/users.update'),
+    Route::post('/api/users.suspend')->action(UsersSuspendAction::class)->name('api/users.suspend'),
+    Route::post('/api/users.delete')->action(UsersDeleteAction::class)->name('api/users.delete'),
+
+    Route::post('/api/groups.list')->action(GroupsListAction::class)->name('api/groups.list'),
+    Route::post('/api/groups.info')->action(GroupsInfoAction::class)->name('api/groups.info'),
+    Route::post('/api/groups.create')->action(GroupsCreateAction::class)->name('api/groups.create'),
+    Route::post('/api/groups.update')->action(GroupsUpdateAction::class)->name('api/groups.update'),
+    Route::post('/api/groups.delete')->action(GroupsDeleteAction::class)->name('api/groups.delete'),
+    Route::post('/api/groups.users')->action(GroupsUsersListAction::class)->name('api/groups.users'),
+    Route::post('/api/groups.add_user')->action(GroupsAddUserAction::class)->name('api/groups.add_user'),
+    Route::post('/api/groups.remove_user')->action(GroupsRemoveUserAction::class)->name('api/groups.remove_user'),
 
     Route::post('/api/collections.list')->action(CollectionsListAction::class)->name('api/collections.list'),
     Route::post('/api/collections.info')->action(CollectionsInfoAction::class)->name('api/collections.info'),
