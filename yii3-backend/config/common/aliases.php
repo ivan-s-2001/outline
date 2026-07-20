92625df3f85f6b6a2b51cefc2a119fdd93da2925
ivan-s-2001 <ivan.s.2001@yandex.com>
2026-07-20 18:04:44 +0300

    Add Yii3 path aliases

---
+++ yii3-backend/config/common/aliases.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    '@root' => dirname(__DIR__, 2),
+    '@public' => '@root/public',
+    '@runtime' => '@root/runtime',
+    '@src' => '@root/src',
+    '@tests' => '@root/tests',
+    '@vendor' => '@root/vendor',
+];
