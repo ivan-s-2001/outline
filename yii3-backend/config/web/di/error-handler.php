15fe2b9d3404f3c73957db23e5033ff031035d17
ivan-s-2001 <ivan.s.2001@yandex.com>
2026-07-20 18:06:54 +0300

    Use JSON error responses

---
+++ yii3-backend/config/web/di/error-handler.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+use Yiisoft\ErrorHandler\Renderer\JsonRenderer;
+use Yiisoft\ErrorHandler\ThrowableRendererInterface;
+
+return [
+    ThrowableRendererInterface::class => JsonRenderer::class,
+];
